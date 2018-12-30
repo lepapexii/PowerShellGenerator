b251334194f00bcd7f84a62e7ae7a1aeb677117a
lepapexii <37328041+lepapexii@users.noreply.github.com>
2018-12-30 18:34:19 -0500

    Add files via upload

---
+++ NetConsole/NetConsole/Hall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace NetConsole
+{
+    class Hall
+    {
+        static void Main(string[] args)
+        {
+            //var  para = args[0].ToString();
+            starter();
+
+
+        }
+
+        static void starter([Optional] string options)
+        {
+
+
+            PSFactory PS = new PSFactory();
+            PS.FireNForget();
+        }
+
+    }
+}
---
+++ NetConsole/NetConsole/PSFactory.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NetConsole
+{
+    public class PSFactory
+    {
+        public PowerShell PowerShellz;
+        private PowerShell PowerShellInstance
+        {
+            get { return this.PowerShellz; }
+            set { this.PowerShellz = value; }
+        }
+        public PSFactory([Optional] string profile)
+        {
+
+
+
+        }
+
+
+        //SYNChronous
+        public void FireNForget()
+        {
+
+     
+
+            using (this.PowerShellz)
+            {
+                PowerShellz = PowerShell.Create();
+
+                // invoke execution on the pipeline (ignore output) Synchronous execution
+                //generate empty PS pipeline
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                PowerShellz.AddScript("Set-ExecutionPolicy 'AllSigned'");
+                PowerShellz.AddScript("param($param1) $d = get-date; $s = 'test string value'; " +
+                      "$d; $s; $param1; get-service");
+
+               
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                PowerShellz.AddParameter("param1", "parameter 1 value!");
+                PowerShellz.Invoke();
+
+            }
+
+            using (this.PowerShellz)
+            {
+                PowerShellz = PowerShell.Create();
+                var go = PowerShellz.AddCommand("Set-ExecutionPolicy");
+                go.InvocationStateInfo.Reason.Message.ToString();
+                PowerShellz.Invoke();
+
+            }
+
+
+
+        }
+
+        //SYNChronous
+        public void CustomFire()
+        {
+
+            // invoke execution on the pipeline (collecting output)
+            // Collection<PSObject> PSOutput = pipe.Invoke();
+
+            // loop through each output object item
+            // foreach (PSObject outputItem in PSOutput)
+            {
+                // if null object was dumped to the pipeline during the script then a null
+                // object may be present here. check for null to prevent potential NRE.
+                //   if (outputItem != null)
+                {
+                    //TODO: do something with the output item 
+                    // outputItem.BaseOBject
+                    //      Console.WriteLine(outputItem.BaseObject.GetType().FullName);
+                    //   Console.WriteLine(outputItem.BaseObject.ToString() + "\n");
+                }
+            }
+
+            VerifyOutput();
+        }
+
+        private void VerifyOutput()
+        {
+
+            // invoke execution on the pipeline (collecting output)
+            // Collection<PSObject> PSOutput = pipe.Invoke();
+
+            // check the other output streams (for example, the error stream)
+            //    if (pipe.Streams.Error.Count > 0)
+            {
+                // error records were written to the error stream.
+                // do something with the items found.
+            }
+        }
+    }
+}
